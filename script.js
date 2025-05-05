faa6b983af8dc98599bd067228c1127204e0fda0
Otavio <otaviobarbosa2004@gmail.com>
2025-05-05 17:16:34 -0300

    Ajuste na acessibilidade do site

--- script.js
+++ script.js
@@ -6,35 +6,25 @@ document.getElementById('preto-e-branco').addEventListener
 );
 
 
-window.onload = function()
-{
+window.onload = function() {
     var corpoDaPagina = document.querySelector('body');
     var aumentarTexto = document.getElementById('aumentar-texto');
     var diminuirTexto = document.getElementById('diminuir-texto');
 
     var fontSize = 100;
-
     var qntAumentarDiminuir = 10;
 
-    aumentarTexto.addEventListener
-    ('click', function(event)
-    {
-        fontSize = fontSize + qntAumentarDiminuir;
+    aumentarTexto.addEventListener('click', function(event) {
+        fontSize += qntAumentarDiminuir;
         corpoDaPagina.style.fontSize = fontSize + '%';
+    });
 
-    }
-);
+    diminuirTexto.addEventListener('click', function(event) {
+        fontSize -= qntAumentarDiminuir;
+        corpoDaPagina.style.fontSize = fontSize + '%';
+    });
+};
 
-diminuirTexto.addEventListener
-('click', function(event)
-{
-    fontSize = fontSize - qntAumentarDiminuir;
-    corpoDaPagina.style.fontSize = fontSize + '%';
-
-}
-);
-
-}
 
 document.getElementById('alto-contraste').addEventListener
 ('click', function()
