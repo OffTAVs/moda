abeb6c722a13ea293903066abaa1ff3f599475c8
Otavio <otaviobarbosa2004@gmail.com>
2025-05-04 23:43:16 -0300

    Adição de acessibilidade

---
+++ script.js
@@ -0,0 +1,45 @@
+document.getElementById('preto-e-branco').addEventListener
+('click', function()
+{
+    document.body.classList.toggle('preto-e-branco');
+}
+);
+
+
+window.onload = function()
+{
+    var corpoDaPagina = document.querySelector('body');
+    var aumentarTexto = document.getElementById('aumentar-texto');
+    var diminuirTexto = document.getElementById('diminuir-texto');
+
+    var fontSize = 100;
+
+    var qntAumentarDiminuir = 10;
+
+    aumentarTexto.addEventListener
+    ('click', function(event)
+    {
+        fontSize = fontSize + qntAumentarDiminuir;
+        corpoDaPagina.style.fontSize = fontSize + '%';
+
+    }
+);
+
+diminuirTexto.addEventListener
+('click', function(event)
+{
+    fontSize = fontSize - qntAumentarDiminuir;
+    corpoDaPagina.style.fontSize = fontSize + '%';
+
+}
+);
+
+}
+
+document.getElementById('alto-contraste').addEventListener
+('click', function()
+{
+document.body.classList.toggle('alto-contraste');
+}
+);
+
